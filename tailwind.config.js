0137b290d66cb1f106c95ee9cd1e267532014d74
omes <zikosalasa6@gmail.com>
2023-10-09 13:58:25 +0900

    first init github

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["index.html","dosen.html"],
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
